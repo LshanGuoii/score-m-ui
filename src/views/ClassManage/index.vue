8b6d16f9aca80b75e106a12c7df6082bb105bd57
luoshang <1215405694@qq.com>
2022-04-02 00:44:01 +0800

    表格

---
+++ src/views/ClassManage/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="app-container user-message">
+    <div></div>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%"
+      :default-sort="{ prop: 'date', order: 'descending' }"
+    >
+      <el-table-column prop="address" label="所属系部" />
+      <el-table-column prop="address" label="所属专业" />
+      <el-table-column prop="name" label="班级名称" sortable />
+      <el-table-column prop="address" label="人数" sortable />
+      <el-table-column prop="address" label="创建时间 " />
+    </el-table>
+
+    <el-pagination
+      :current-page="pageInit.pageNum"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageInit.pageSize"
+      layout="->, total, sizes, prev, pager, next, jumper"
+      :total="pageInit.total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserManage",
+
+  components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped></style>
